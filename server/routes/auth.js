cd6314bdf34538fad3640b4cd34eb64749339a8f
Dev Prasad <devpatra2000@gmail.com>
2025-08-21 18:00:06 +0530

    Comment Added in Auth routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,13 @@ const express = require('express')
 const router = express.Router()
 const { signup, signin } = require('../controllers/authController')
 
+
+//auth routes
+
 router.post('/signup', signup)
 router.post('/signin', signin)
 
 module.exports = router
+
+
+
